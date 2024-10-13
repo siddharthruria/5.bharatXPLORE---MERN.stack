b664727cb52dab17a8299e0cdf7495a162136446
Siddharth Ruria <siddharthruria10@gmail.com>
2024-10-13 19:10:19 +0530

    created user context

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
+  
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="container position-relative">
+      <h2 className="my-4 position-absolute start-50 translate-middle">
+        login to open tasks
+      </h2>
+      <form className="py-5 my-4">
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="btn btn-primary my-3">
+            submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignupPage = () => {
-  return (
-    <div>SignupPage</div>
-  )
-}
+  const [credentials, setCredentials] = useState({
+    username: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
 
-export default SignupPage
+  return (
+    // signup form taken straight from bootstrap website
+    <div className="container position-relative">
+      <h2 className="my-4 position-absolute start-50 translate-middle">
+        signup to start contributing!
+      </h2>
+      <form className="py-5 my-4">
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            minLength={3}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            email address
+          </label>
+          <input type="email" className="form-control" id="email" required />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label">
+            confirm password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="cpassword"
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="btn btn-primary my-3">
+            submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
